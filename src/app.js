be5fb22649c6261435ec5948d2b276b6d95c14e3
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2026-01-27 01:31:22 +0530

    Updated app removed secure from sessionOption

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ const sessionOption = {
         expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge : 7 * 24 * 60 * 60 * 1000,
         httpOnly : true,
-        secure: process.env.NODE_ENV === 'production',
+        // secure: process.env.NODE_ENV === 'production',
     }
 };
 
