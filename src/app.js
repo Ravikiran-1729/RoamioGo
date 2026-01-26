8e7f188940c5daa96636346d996bff3e72f6237a
Ravikiran-1729 <ravikiranpyawalkar@gmail.com>
2026-01-27 01:34:52 +0530

    Updated app added secure from sessionOption

--- src/app.js
+++ src/app.js
@@ -39,19 +39,33 @@ store.on('error', (err) => {
 });
 
 
+// const sessionOption = {
+//     store,
+//     secret : process.env.SESSION_SECRET,
+//     resave : false,
+//     saveUninitialized : false,
+//     cookie : {
+//         expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
+//         maxAge : 7 * 24 * 60 * 60 * 1000,
+//         httpOnly : true,
+//         // secure: process.env.NODE_ENV === 'production',
+//     }
+// };
+
 const sessionOption = {
     store,
-    secret : process.env.SESSION_SECRET,
-    resave : false,
-    saveUninitialized : false,
-    cookie : {
-        expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge : 7 * 24 * 60 * 60 * 1000,
-        httpOnly : true,
-        // secure: process.env.NODE_ENV === 'production',
+    name: 'roamioGo.sid',
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    proxy: true,
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: 7 * 24 * 60 * 60 * 1000
     }
 };
-
 
 app.use(cookieParser());
 app.use(session(sessionOption));
