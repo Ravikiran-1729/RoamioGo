f9a08a6640e5d9be138e751bac52c63d888af304
Ravikiran Pradip Yavalkar <ravikiranpyawalkar@gmail.com>
2026-06-22 15:28:04 +0530

    Refactor connectDB function with error handling
    Updated the connectDB function to include error handling and changed the success message.

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -10,9 +10,19 @@ const mongoose = require('mongoose');
 // const MONGOURL = process.env.MONGOURL;
 const ATLASURL = process.env.ATLAS_URL;
 
-const connectDB = async () =>{
+// const connectDB = async () =>{
+//     await mongoose.connect(ATLASURL);
+//     console.log("✅ Database Connected Successfully");
+// };
+const connectDB = async () => {
+  try {
+    console.log("Connecting to MongoDB...");
     await mongoose.connect(ATLASURL);
-    console.log("✅ Database Connected Successfully");
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB Error:", err);
+    throw err;
+  }
 };
 
 module.exports = connectDB;
